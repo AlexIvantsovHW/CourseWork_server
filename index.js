aeac040951656ceda69bb5187d6e5750ad2aabf8
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2023-08-24 16:41:36 +0300

    Login page is updated

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app=express();
 const cors = require('cors')
 const port=process.env.PORT||3001
 const bodyParser=require('body-parser')
-const mysql=require('mysql')
+
 var multer = require("multer");
 var upload = multer({ dest: "./upload/" });
+const mysql=require('mysql')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json());
@@ -16,30 +17,26 @@ app.use((req, res, next) => {
     next();
   })
 app.use(cors({origin:"*"}));
-app.get('/',(req,res)=>{
-    res.send('Hi world!')
-})
+app.use(require('./routes'))
+
+//---------------------------------------
 const conn=mysql.createConnection({
     host:'bblqpq3zqeki4vqkexwc-mysql.services.clever-cloud.com',
     user:'ueedenphzx2yvgfe',
     database:'bblqpq3zqeki4vqkexwc',
     password:'Hea5c7Ksi0Ls7h3kyZzp'
 })
-app.get('/users',(req,res)=>{   
-    let sql="SELECT * FROM Users";
-    conn.query(sql,(err,result)=>{
-        if(err){console.log(err);
-        }else{res.send(result)}
-    })
-})
-app.post('/registration',upload.array(),(req,res)=>{  
-    const name=req.body.name;
-    const email=req.body.email;
-    const password=req.body.pass;
 
-    let qUpdate="INSERT INTO `Users`(`name`,`email`,`password`) VALUES(?,?,?)"
-    conn.query(qUpdate,[name,email,password],(err)=>{console.log(err);})
-    console.log('done')
-})
-
+sql="SELECT * FROM Users";
+conn.query("SELECT * FROM  Users WHERE `name`='Alina'", function (err, result, fields) {
+    if (err) throw err;
+    if(result[0].name==='Alina'){
+        if(result[0].password==='bAlochka'){
+            console.log(result);
+        }else{console.log('Password is wrong!')}
+    }else
+        { console.log(result[0].name)
+            console.log('User undefined')}
+    });
+;
 app.listen(port,()=>{console.log(`App is started, port: ${port}`)})
---
+++ routes/index.js
@@ -0,0 +1,50 @@
+const express=require('express')
+const router=express.Router();
+var multer = require("multer");
+var upload = multer({ dest: "./upload/" });
+const mysql=require('mysql')
+
+const conn=mysql.createConnection({
+    host:'bblqpq3zqeki4vqkexwc-mysql.services.clever-cloud.com',
+    user:'ueedenphzx2yvgfe',
+    database:'bblqpq3zqeki4vqkexwc',
+    password:'Hea5c7Ksi0Ls7h3kyZzp'
+})
+
+router.get('/',(req,res)=>{
+    res.send('Hi world!')
+})
+
+router.get('/users',(req,res)=>{   
+    let sql="SELECT * FROM Users";
+    conn.query(sql,(err,result)=>{
+        if(err){console.log(err);
+        }else{res.send(result)}
+    })
+})
+router.post('/registration',upload.array(),(req,res)=>{  
+    const name=req.body.name;
+    const email=req.body.email;
+    const password=req.body.pass;
+    let qUpdate="INSERT INTO `Users`(`name`,`email`,`password`) VALUES(?,?,?)"
+    conn.query(qUpdate,[name,email,password],(err)=>{console.log(err);})
+    console.log('done')
+})
+
+router.post('/login',upload.array(),(req,res)=>{  
+    const name=req.body.name;
+    const password=req.body.pass;
+    let sql="SELECT * FROM  Users WHERE `name`=?";
+    conn.query(sql,[name], function (err, result) {
+        if (err) throw err;
+        if(result[0].name==='Alina'){
+            if(result[0].password===password){
+                res.send(true);
+            }else{res.send('Password is wrong!')}
+        }else
+            {res.send('User undefined')}});
+    ;
+
+})
+
+module.exports=router;
