553d4c90c9c8d1c34780543150d3b61a78d6505c
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2023-08-27 19:57:32 +0300

    profile requests are created

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const cors = require('cors')
 const port=process.env.PORT||3001
 const bodyParser=require('body-parser')
 
+//
+var multer = require("multer");
+var upload = multer({ dest: "./upload/" });
+const mysql=require('mysql')
+
+const conn=mysql.createConnection({
+    host:'bblqpq3zqeki4vqkexwc-mysql.services.clever-cloud.com',
+    user:'ueedenphzx2yvgfe',
+    database:'bblqpq3zqeki4vqkexwc',
+    password:'Hea5c7Ksi0Ls7h3kyZzp'
+})
+//
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json());
 app.use((req, res, next) => {
@@ -15,5 +28,14 @@ app.use((req, res, next) => {
 app.use(cors({origin:"*"}));
 app.use(require('./routes'))
 
+
+/*   const id=7
+  conn.connect(function(err) {
+    if (err) throw err;
+    conn.query("SELECT * FROM Recommendation WHERE `id_user`=7", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }); */
 
 app.listen(port,()=>{console.log(`App is started, port: ${port}`)})
--- routes/index.js
+++ routes/index.js
@@ -40,12 +40,39 @@ router.post('/login',upload.array(),(req,res)=>{
         if(result[0].name===name){
             if(result[0].password===password){
                 console.log(result[0].password===password);
-                res.send(true);
+                res.send({name:result[0].name,password:result[0].password,id:result[0].id,id_r:result[0].id_r,auth:true});
             }else{
                 res.send('Password is wrong!')}
         }else
             {   console.log(result[0].password===password);
                 res.send('User undefined')}});
 })
+router.post('/recommendation',upload.array(),(req,res)=>{   
+    const id=req.body.id
+    let sql="SELECT * FROM Recommendation WHERE `id_user`=?";
+    conn.query(sql,[id],(err,result)=>{
+        if(err){console.log(err);
+        }else{
+            console.log(result)
+            res.send(result)}
+    })
+  })
+  let qUpdate="INSERT INTO `dataTable`(`name`,`email`,`tReg`,`status`,`password`) VALUES(?,?,?,?,?)"
+  conn.query(qUpdate,[name,email,tReg,'Active',pass],(err)=>{console.log(err);})
 
+  router.post('/addRecommendation',upload.array(),(req,res)=>{   
+    const id_user=req.body.id_user;
+    const recommendation=req.body.recommendation;
+    const group_name=req.body.group_name;
+    const recommend_name=req.body.recommend_name;
+    const recommend_category=req.body.recommend_category;
+        
+    let sql="INSERT INTO Recommendation WHERE (`id_user`,`recommendation`,`group_name`,`recommend_name`,`recommend_category`) VALUES (?,?,?,?,?)";
+    conn.query(sql,[id_user,recommendation,group_name,recommend_name,recommend_category],(err,result)=>{
+        if(err){console.log(err);
+        }else{
+            console.log(result)
+            res.send(result)}
+    })
+  }) 
 module.exports=router;
