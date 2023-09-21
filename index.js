7659108ad51273d716f1ad295f76300da55af8c6
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2023-09-21 17:48:56 +0300

    project is done

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-/* import {config} from 'dotenv'; */
 const express=require('express');
 const app=express();
 const cors = require('cors')
 const port=process.env.PORT||3001
-/* const passport = require('passport'); */
 const bodyParser=require('body-parser')
 const authRoute=require("./routes/auth")
 const multer = require("multer");
 const upload = multer({ dest: "./upload/" });
 const cloudinary=require('cloudinary').v2
-/* const RecommenRoute=require("./routes/Recommend") */
-/* const passportSetup=require('./passport') */
 const cookieSession = require('cookie-session')
 require('dotenv').config()
-/* require('./passport'); */
+
 
 cloudinary.config({ 
   cloud_name: 'dj4jyuhfj', 
@@ -31,15 +27,7 @@ app.use((req, res, next) => {
 app.use(cors({origin:"*"}));
 app.use(require('./routes'))
 
-/* app.use(cookieSession({
-  name: "session",
-  keys: ["cyberwolve"],
-  maxAge:24*60*60*100,
-}))
-app.use(passport.initialize());
-app.use(passport.session()); */
 app.use("/auth",authRoute);
-/* app.use("/recommend",RecommenRoute); */
 
 app.listen(port,()=>{console.log(`App is started, port: ${port}`)})
 
